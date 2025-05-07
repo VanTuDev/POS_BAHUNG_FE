71382c777fd74278662827cd0e9babba51d6b252
VanTuDev <Vantu.dev@gmail.com>
2025-05-07 14:20:56 +0700

    update code

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-13fdf7ef'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.08huv33p0lg"
+    "revision": "0.oi5td6m4qhg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
